968cdbc6f408379c0c18c514afe29ca4c6d71b19
ValentinoChiappanni <valentino.chiappanni@estudiantes.unahur.edu.ar>
2025-03-22 12:41:34 -0300

    cambios

--- src/index.js
+++ src/index.js
@@ -5,13 +5,13 @@ const db = require("./models");
 const app = express();
 const cors = require("cors");
 const PORT = 4000;
-
+const API_PREFIX = "/unahur-api";
 app.use(cors());
 app.use(express.json());
 
 app.use(genericMiddleware.requestTime);
-app.use(routes.seriesRoute);
-app.use(routes.usuariosRoute);
+app.use(API_PREFIX,routes.seriesRoute);
+app.use(API_PREFIX,routes.usuariosRoute);
 app.listen(PORT, async () => {
   console.log(`Aplicacion iniciada en el puerto ${PORT}`);
   //Esto lo hacemos solo en desarrollo para sincronizar el modelo con la db
